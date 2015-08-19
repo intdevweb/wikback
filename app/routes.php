d5a13e72e8242ed3a9381adb527b5ba0efe2b159
intdevweb <jp_informatique@hotmail.fr>
2015-08-19 15:37:56 +0200

    add new path mot du jour

--- app/routes.php
+++ app/routes.php
@@ -3,4 +3,6 @@
 	$w_routes = array(
 		['GET', '/admin/termes/', 'Term#showAll', 'show_all_terms'],
 		['GET', '/admin/termes/suppression/[i:id]/', 'Term#delete', 'delete_term'],
+		['GET|POST', '/admin/termes/modification/[i:id]/', 'Term#edit', 'edit_term'],
+		['GET', '/admin/termes/nouveau-mdj/', 'Term#changewodt', 'changewotd'],
 		);
